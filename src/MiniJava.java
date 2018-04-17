9389deada2bbc7b88931e719a1a491f2e2f0c1c7
7PH <b.raymond@protonmail.com>
2018-04-17 18:01:49 +0200

    issou

--- src/MiniJava.java
+++ src/MiniJava.java
@@ -1,3 +1,36 @@
-public class MiniJava {
 
+
+interface A {
+    void doA();
+}
+interface B<T extends A> {
+    T doB(T a);
+}
+
+class TheA implements A {
+    @Override
+    public void doA() {
+        System.out.println("A!");
+    }
+}
+
+class TheB<T extends A> extends TheA implements B<T> {
+    @Override
+    public T doB(T a) {
+        a.doA();
+        return a;
+    }
+}
+
+class TheC<T extends A & B> {
+    public void doC (T obj) {
+        obj.doA();
+        obj.doB(obj);
+    }
+}
+
+class MiniJava {
+    public static void main(String args[]) {
+        TheC<TheB> myC = new TheC<>();
+    }
 }
