2e2189e78a27a7b846fbebd34b9ce91d57e183cc
7PH <b.raymond@protonmail.com>
2018-04-30 23:24:03 +0200

    Improved test script

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
-./build.sh > /dev/null;
+#!/bin/bash
+
+echo -n "Building..              ";
+ant > /dev/null;
+echo " [OK]";
+
+echo -n "Compiling test file..   ";
 tsc;
+echo " [OK]";
+
 mocha tests/test.js --reporter $1;
