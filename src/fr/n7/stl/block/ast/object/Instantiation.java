2d9781e8bafd57c19daa58256e561cf4e099d5ca
Nicolas <nicolas.dureisseix@etu.enseeiht.fr>
2018-05-18 17:52:23 +0200

    Some things fixed.

--- src/fr/n7/stl/block/ast/object/Instantiation.java
+++ src/fr/n7/stl/block/ast/object/Instantiation.java
@@ -3,7 +3,11 @@ package fr.n7.stl.block.ast.object;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Instantiation {
+import fr.n7.stl.block.ast.scope.Declaration;
+import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.type.Type;
+
+public class Instantiation implements Type {
 	
 	private String name;
 	
@@ -17,6 +21,36 @@ public class Instantiation {
 	public Instantiation(String _name, List<Instantiation> _instantiations) {
 		this.name = _name;
 		this.instantiations = _instantiations;
+	}
+
+	@Override
+	public boolean equalsTo(Type _other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean compatibleWith(Type _other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Type merge(Type _other) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean resolve(HierarchicalScope<Declaration> _scope) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
