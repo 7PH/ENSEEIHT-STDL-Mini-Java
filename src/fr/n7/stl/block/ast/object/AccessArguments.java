5f4ea1088b3b5b1bfc25af1d1f07c7e22c35bd9e
Nicolas <nicolas.dureisseix@etu.enseeiht.fr>
2018-05-21 13:12:59 +0200

    Added toString method in enum classes.

--- src/fr/n7/stl/block/ast/object/AccessArguments.java
+++ src/fr/n7/stl/block/ast/object/AccessArguments.java
@@ -3,4 +3,16 @@ package fr.n7.stl.block.ast.object;
 public enum AccessArguments {
 	Public,
 	Private;
+	
+	@Override
+	public String toString() {
+		switch(this) {
+			case Public :
+				return "public";
+			case Private : 
+				return "private";
+			default :
+				throw new IllegalArgumentException( "The default case should never be triggered."); 
+		}
+	}
 }
--- src/fr/n7/stl/block/ast/object/ClassArguments.java
+++ src/fr/n7/stl/block/ast/object/ClassArguments.java
@@ -2,5 +2,17 @@ package fr.n7.stl.block.ast.object;
 
 public enum ClassArguments {
 	Final,
-	Abstract,
+	Abstract;
+	
+	@Override
+	public String toString() {
+		switch(this) {
+			case Final :
+				return "final";
+			case Abstract :
+				return "abstract";
+			default :
+				throw new IllegalArgumentException( "The default case should never be triggered.");
+		}
+	}
 }
