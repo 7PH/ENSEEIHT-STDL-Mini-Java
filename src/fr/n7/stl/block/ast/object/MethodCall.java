82296a7deeddb1e2f33191bfc6e60edca98d7c83
Nicolas <nicolas.dureisseix@etu.enseeiht.fr>
2018-05-22 22:00:56 +0200

    MethodCall checkType.

--- src/fr/n7/stl/block/ast/object/MethodCall.java
+++ src/fr/n7/stl/block/ast/object/MethodCall.java
@@ -3,12 +3,15 @@ package fr.n7.stl.block.ast.object;
 import fr.n7.stl.block.ast.SemanticsUndefinedException;
 import fr.n7.stl.block.ast.expression.Expression;
 import fr.n7.stl.block.ast.instruction.Instruction;
+import fr.n7.stl.block.ast.instruction.declaration.ParameterDeclaration;
 import fr.n7.stl.block.ast.scope.Declaration;
 import fr.n7.stl.block.ast.scope.HierarchicalScope;
+import fr.n7.stl.block.ast.type.AtomicType;
 import fr.n7.stl.block.ast.type.Type;
 import fr.n7.stl.tam.ast.Fragment;
 import fr.n7.stl.tam.ast.Register;
 import fr.n7.stl.tam.ast.TAMFactory;
+import fr.n7.stl.util.Logger;
 
 import java.util.List;
 
@@ -16,14 +19,16 @@ public class MethodCall implements Instruction, Expression {
 
     private String identifier;
 
-    private Expression object;
+    private Expression objectIdentifier;
 
     private String method;
+    
+    private MethodDefinition methodDefinition;
 
     private List<Expression> parameters;
 
     public MethodCall(Expression object, String method, List<Expression> parameters) {
-        this.object = object;
+        this.objectIdentifier = object;
         this.method = method;
         this.parameters = parameters;
     }
@@ -46,8 +51,35 @@ public class MethodCall implements Instruction, Expression {
 
     @Override
     public boolean checkType() {
-    	// TODO //
-    	throw new SemanticsUndefinedException("checkType method is undefined for MethodCall.");
+    	boolean b = false;
+    	List<ParameterDeclaration> declaredParameters = this.methodDefinition.getSignature().getParameters();
+    	int numOfPar = this.parameters.size();
+    	
+    	// Verify if the number of parameters is correct
+    	if (numOfPar == declaredParameters.size()) {
+    		for (int i = 0; i < numOfPar; i++) {
+    			Type parameterType = this.parameters.get(i).getType();
+    			Type declaredType = declaredParameters.get(i).getType();
+    			// Verify if a type is an ErrorType
+    			if (!parameterType.equalsTo(AtomicType.ErrorType)) {
+    				// Verify compatibility between declared and used type
+    				b &= parameterType.compatibleWith(declaredType);
+    				if (!b) {
+    				Logger.error("Parameter " + parameterType.toString() + " use in "
+    							+ this.identifier + " method is not compatible with declared one : " + declaredParameters + ".");
+    				}
+
+    			} else {
+    				Logger.error("Parameter " + parameterType.toString() + " use in "
+							+ this.identifier + "method is an ErrorType.");
+    			}
+    		}
+    		
+    	} else {
+    		Logger.error("Call of " + this.identifier + " but found " + numOfPar + " and expected " + declaredParameters.size() + ".");
+    	}
+    	
+    	return b;
     }
 
     @Override
@@ -67,6 +99,6 @@ public class MethodCall implements Instruction, Expression {
 
     @Override
     public String toString() {
-        return object + "." + method + "(" + parameters + ");";
+        return objectIdentifier + "." + method + "(" + parameters + ");";
     }
 }
