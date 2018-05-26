cd3112778b6cb299956bbf64361a9a0b5aca0b6c
Nicolas <nicolas.dureisseix@etu.enseeiht.fr>
2018-05-26 22:32:53 +0200

    Introducing DefinitionAccess.

--- src/fr/n7/stl/block/ast/expression/assignable/AttributeAssignment.java
+++ src/fr/n7/stl/block/ast/expression/assignable/AttributeAssignment.java
@@ -4,7 +4,9 @@ import fr.n7.stl.block.ast.expression.AbstractAttribute;
 import fr.n7.stl.block.ast.expression.Expression;
 import fr.n7.stl.block.ast.object.AttributeDefinition;
 import fr.n7.stl.block.ast.object.InstanceType;
+import fr.n7.stl.block.ast.type.Type;
 import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.Register;
 import fr.n7.stl.tam.ast.TAMFactory;
 
 public class AttributeAssignment extends AbstractAttribute implements AssignableExpression {
@@ -22,5 +24,20 @@ public class AttributeAssignment extends AbstractAttribute implements Assignable
 	public Fragment getCode(TAMFactory factory) {
         Fragment fragment = factory.createFragment();
         return fragment;
-	}
+    }
+
+    @Override
+    public int allocateMemory(Register register, int offset) {
+        return 0;
+    }
+
+    @Override
+    public boolean checkType() {
+        return false;
+    }
+
+    @Override
+    public Type getReturnType() {
+        return null;
+    }
 }
